e68e8203acbee3eb88d89ca905beb94f03714842
Mari <merisid2399@mail.ru>
2022-03-26 23:16:08 +0300

    init

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Alex", "Smith", (byte) 37);
+        userService.saveUser("Bill", "Jones", (byte) 74);
+        userService.saveUser("Nick", "Davis", (byte) 59);
+        userService.saveUser("Steven", "Miller", (byte) 22);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,36 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Connection conn = null;
+    private static Util instance = null;
+
+    private final String URL = "jdbc:mysql://localhost:3307/newbase";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "1357";
+
+    private Util() {
+        try {
+            if (null == conn || conn.isClosed()) {
+                conn = DriverManager
+                        .getConnection(URL, USERNAME,PASSWORD);
+            }
+        } catch (SQLException e ) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Util getInstance() {
+        if (null == instance) {
+            instance = new Util();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
 }
