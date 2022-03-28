787613bf44864970595d9b50d46fc585ef2028ba
Mari <merisid2399@mail.ru>
2022-03-28 11:42:30 +0300

    autocommit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -15,7 +15,9 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void createUsersTable() {
         try (PreparedStatement pstm = conn.prepareStatement(SQL_CREATE)) {
+            conn.setAutoCommit(false);
             pstm.execute();
+            conn.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -23,7 +25,9 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void dropUsersTable() {
         try (PreparedStatement pstm = conn.prepareStatement("DROP TABLE IF EXISTS users")) {
+            conn.setAutoCommit(false);
             pstm.execute();
+            conn.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -31,10 +35,12 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void saveUser(String name, String lastName, byte age) {
         try (PreparedStatement pstm = conn.prepareStatement("INSERT INTO users (name, last_name, age) VALUES (?, ?, ?)")) {
+            conn.setAutoCommit(false);
             pstm.setString(1, name);
             pstm.setString(2, lastName);
             pstm.setByte(3, age);
             pstm.executeUpdate();
+            conn.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -42,8 +48,10 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void removeUserById(long id) {
         try (PreparedStatement pstm = conn.prepareStatement("DELETE FROM users WHERE id = ?")) {
+            conn.setAutoCommit(false);
             pstm.setLong(1, id);
             pstm.executeUpdate();
+            conn.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -53,12 +61,14 @@ public class UserDaoJDBCImpl implements UserDao {
         List<User> users = new ArrayList<>();
 
         try (ResultSet resultSet = conn.createStatement().executeQuery("SELECT * FROM users")) {
+            conn.setAutoCommit(false);
             while(resultSet.next()) {
                 User user = new User(resultSet.getString("name"),
                         resultSet.getString("last_name"), resultSet.getByte("age"));
                 user.setId(resultSet.getLong("id"));
                 users.add(user);
             }
+            conn.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -68,7 +78,9 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void cleanUsersTable() {
         try (PreparedStatement pstm = conn.prepareStatement("TRUNCATE TABLE users")) {
+            conn.setAutoCommit(false);
             pstm.execute();
+            conn.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
